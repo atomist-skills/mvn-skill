775f4066fa9f8cae3970a44a23899dde3d8d1352
Christian Dupuis <cd@atomist.com>
2020-12-01 10:42:19 +0100

    Remove some old code

--- lib/git.ts
+++ lib/git.ts
@@ -15,62 +15,6 @@
  */
 
 import { subscription } from "@atomist/skill";
-import * as semver from "semver";
-
-/**
- * Remove non-tag-worthy, non-semver-prerelease characters from git
- * branch.
- */
-export function cleanGitRef(refName: string): string {
-	return refName
-		.replace(/\//g, "-")
-		.replace(/_/g, "-")
-		.replace(/[^0-9A-Za-z.-]/g, "")
-		.replace(/\.+/g, ".");
-}
-
-/** Return cleaned name prepended with `prefix-`. */
-export function gitRefToNpmTag(branchName: string, prefix = "branch"): string {
-	return `${prefix}-${cleanGitRef(branchName)}`;
-}
-
-export interface NextPrereleaseTagArgs {
-	/** Current branch */
-	branch: string;
-	/** Repository default branch */
-	defaultBranch: string;
-	/** Next release version */
-	nextReleaseVersion: string;
-	/** Current tags */
-	tags: string[];
-}
-
-/**
- * Return the next prerelease semantic version tag.
- */
-export function nextPrereleaseTag(args: NextPrereleaseTagArgs): string {
-	if (args.tags.includes(args.nextReleaseVersion)) {
-		throw new Error(
-			`Current tags already include next release version: ${args.nextReleaseVersion}`,
-		);
-	}
-	const semverTags = args.tags.filter(t => semver.valid(t));
-	const cleanBranch = cleanGitRef(args.branch);
-	const prereleaseBranch =
-		args.branch === args.defaultBranch
-			? cleanBranch
-			: `branch-${cleanBranch}`;
-	const nextVersion = args.nextReleaseVersion;
-	const prefixRegExp = RegExp(
-		`^${nextVersion}-${prereleaseBranch}\\.(?:0|[1-9]\\d*)$`,
-	);
-	const matchingTags = semverTags.filter(t => prefixRegExp.test(t));
-	const sortedTags = matchingTags.sort((t1, t2) => semver.compare(t2, t1));
-	if (sortedTags.length < 1) {
-		return `${nextVersion}-${prereleaseBranch}.0`;
-	}
-	return semver.inc(sortedTags[0], "prerelease");
-}
 
 export type EventSubscription =
 	| subscription.types.OnPushSubscription
@@ -100,20 +44,4 @@ export function eventRepo(
 		(data as subscription.types.OnPushSubscription).Push?.[0]?.repo ||
 		(data as subscription.types.OnTagSubscription).Tag?.[0]?.commit?.repo
 	);
-}
-
-/**
- * Extract branch from even data. Will be `undefined` for
- * tag-triggered events.
- */
-export function eventBranch(data: EventSubscription): string | undefined {
-	return (data as subscription.types.OnPushSubscription).Push?.[0]?.branch;
-}
-
-/**
- * Extract branch from even data. Will be `undefined` for
- * commit-triggered events.
- */
-export function eventTag(data: EventSubscription): string | undefined {
-	return (data as subscription.types.OnTagSubscription).Tag?.[0]?.name;
 }
